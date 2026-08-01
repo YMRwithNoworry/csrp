f2d0b47dea153cf3a91a24af9766bb8ade29ebf5
Paojiao134 <168279861+YMRwithNoworry@users.noreply.github.com>
2026-08-01 16:37:52 +0800

    -（修复）强制替换修正朝向的生物模型

--- src/main/java/alku/csrp/client/renderer/PrimitiveParasiteRenderer.java
+++ src/main/java/alku/csrp/client/renderer/PrimitiveParasiteRenderer.java
@@ -1,39 +1,14 @@
 package alku.csrp.client.renderer;
 
 import alku.csrp.client.model.PrimitiveParasiteModel;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Axis;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.world.entity.Mob;
 import software.bernie.geckolib.animatable.GeoEntity;
-import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
-import java.util.Set;
-
 public final class PrimitiveParasiteRenderer<T extends Mob & GeoEntity> extends GeoEntityRenderer<T> {
-    private static final Set<String> REVERSED_MODELS = Set.of(
-            "bomber_light", "haunter", "warden", "wraith", "anc_dreadnaut_ten"
-    );
-
-    private final boolean reverseFacing;
-
     public PrimitiveParasiteRenderer(EntityRendererProvider.Context context, String id, float shadowRadius) {
         super(context, new PrimitiveParasiteModel<>(id));
         this.shadowRadius = shadowRadius;
-        this.reverseFacing = REVERSED_MODELS.contains(id);
-    }
-
-    @Override
-    public void preRender(PoseStack poseStack, T entity, BakedGeoModel model,
-                          MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender,
-                          float partialTick, int packedLight, int packedOverlay, int colour) {
-        if (reverseFacing) {
-            poseStack.mulPose(Axis.YP.rotationDegrees(180.0F));
-        }
-        super.preRender(poseStack, entity, model, bufferSource, buffer, isReRender, partialTick,
-                packedLight, packedOverlay, colour);
     }
 }
