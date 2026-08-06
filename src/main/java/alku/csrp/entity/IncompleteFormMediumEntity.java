7cfd0a0fdd73f1e16a913800e4da8a13ce993afe
Paojiao134 <168279861+YMRwithNoworry@users.noreply.github.com>
2026-08-06 17:48:38 +0800

    （修复）IncompleteFormMediumEntity动画判断逻辑改为基于速度检测

--- src/main/java/alku/csrp/entity/IncompleteFormMediumEntity.java
+++ src/main/java/alku/csrp/entity/IncompleteFormMediumEntity.java
@@ -51,7 +51,7 @@ public final class IncompleteFormMediumEntity extends IncompleteFormSmallEntity 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
         controllers.add(new AnimationController<>(this, "movement_controller", 4,
-                state -> state.setAndContinue(state.isMoving() ? WALK : IDLE)));
+                state -> state.setAndContinue(getDeltaMovement().horizontalDistanceSqr() >= 0.0001 ? WALK : IDLE)));
         controllers.add(new AnimationController<>(this, "attack_controller", 0,
                 state -> PlayState.STOP).triggerableAnim("attack", ATTACK));
     }
